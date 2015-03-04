4d00dde7a45f638b6ff04dc15acf6c497670c65f
GregoryBrookes <gregorybrookes@hotmail.com>
2015-03-03 19:38:13 -0500

    updated Odometry Correction

--- FinalProject/src/navigation/odometry/correction/OdometryCorrection.java
+++ FinalProject/src/navigation/odometry/correction/OdometryCorrection.java
@@ -8,11 +8,23 @@
  */
 package navigation.odometry.correction;
 
+import navigation.Driver;
+import navigation.Navigation;
+import navigation.odometry.Odometer;
+import sensors.FilteredColorSensor;
+
 /**
  * Uses light sensors to correct the odometer's coordinates.
  * Should not have ties to any other classes except the odometer and the light sensors.
- * @author Oleg
+ * @author Gregory Brookes
  */
 public abstract class OdometryCorrection implements Runnable {
+	protected Odometer odo;
+	
+	public OdometryCorrection(Odometer odo) {
+		this.odo = odo;
+	}
 	public abstract void run();
+	
+	
 }
