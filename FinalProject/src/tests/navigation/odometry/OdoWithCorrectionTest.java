804fdae6a68f0a7f963d45ab58a9e4d55073c721
GregoryBrookes <gregorybrookes@hotmail.com>
2015-03-24 12:54:19 -0400

    You though ray's path was cool? try Ray's Complex Path! Beware, you might summon a deamon!

--- FinalProject/src/tests/navigation/odometry/OdoWithCorrectionTest.java
+++ FinalProject/src/tests/navigation/odometry/OdoWithCorrectionTest.java
@@ -8,12 +8,14 @@
  */
 package tests.navigation.odometry;
 
+import lejos.robotics.pathfinding.Path;
 import navigation.Navigation;
 import navigation.odometry.Odometer;
 import navigation.odometry.correction.CorrectionLightSensorSS;
 import tests.TestCase;
 import util.Measurements;
 import util.OdometryDisplay;
+import util.Paths;
 import util.songs.Song;
 import static util.Utilities.pause;
 
@@ -47,25 +49,53 @@ public class OdoWithCorrectionTest extends TestCase {
 		(new Thread(correct)).start();
 		(new Thread(display)).start();
 		
-		(new Thread() {
+	/*	(new Thread() {
 			public void run() {
 				while(true) {
 					System.out.println(odo.getX()+"\t"+odo.getY()+"\t"+odo.getTheta());
 					pause(500);
 				}
 			}
-		}).start();
+		}).start();*/
 
-		raysPath();
+		Paths.square(Measurements.TILE*2);
 		
 		new util.songs.Tetris().play();
 	}
 	
 	
-	private void raysPath() {
+	private void raysSimplePath() {
 		nav.travelTo(Measurements.TILE, 2*Measurements.TILE);
+		System.out.println(odo.getX()+"\t"+odo.getY()+"\t"+odo.getTheta());
 		nav.travelTo(-Measurements.TILE, 2*Measurements.TILE);
+		System.out.println(odo.getX()+"\t"+odo.getY()+"\t"+odo.getTheta());
 		nav.travelTo(-3*Measurements.TILE, 0);
+		System.out.println(odo.getX()+"\t"+odo.getY()+"\t"+odo.getTheta());
 		nav.travelTo(0, 0, 0);
+		System.out.println(odo.getX()+"\t"+odo.getY()+"\t"+odo.getTheta());
+	}
+	private void raysComplexPath() {
+																			printcoordinates();
+		nav.travelTo(Measurements.TILE*2, 1*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*0, 1*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*2, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*1, 2*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*0, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*0, 1*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*1, 2*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*2, 1*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*2, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*0, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*2, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*4, 2*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*6, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*4, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*5, 1*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*3, 1*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*4, 0*Measurements.TILE);				printcoordinates();
+		nav.travelTo(Measurements.TILE*2, 0*Measurements.TILE);				printcoordinates();
+	}
+	private void printcoordinates(){
+		System.out.println(odo.getX()+"\t"+odo.getY()+"\t"+odo.getTheta());
 	}
 }
