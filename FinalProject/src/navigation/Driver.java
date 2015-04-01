99ff2228fdba60059625d7e93442e786857b7033
GregoryBrookes <gregorybrookes@hotmail.com>
2015-04-01 11:10:11 -0400

    Robot calibrated

--- FinalProject/src/navigation/Driver.java
+++ FinalProject/src/navigation/Driver.java
@@ -31,7 +31,7 @@ public class Driver {
 	
 	private final static int DRIFT_FACTOR = 95;				// Bigger = drift tighter , Smaller = larger
 	
-	private final static double WHL_RADIUS = 2.09;			// smaller radius = go further
+	private final static double WHL_RADIUS = 2.12;			// smaller radius = go further
 	private final static double WHL_SEPARATION = 16.6;		// smaller width = turn less
 
 	private final static NXTRegulatedMotor leftMotor = Motor.B, rightMotor = Motor.A;
